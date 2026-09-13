1c1a8542c14866b92d3d6e040a79b6ed4f9dd509
tbiliso <vakhoj@gmail.com>
2026-09-13 20:04:18 +0400

    feat: add font toggle

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
+import { useState } from 'react';
+import LogIn from './pages/Login.jsx'
 function App() {
+
+  const [font, setFont] = useState('HarryPotter')
+  
   return (
-    <div>
+    <div style={{fontFamily:font}}>
+      <input type="checkbox"  className='fontToggle' onChange={(e)=>setFont(e.target.checked ? "HarryPotter":"Arial")}/>
       <LogIn />
     </div>
   );
