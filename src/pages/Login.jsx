b6a76d8d5aa4fff400d96b2959a34047df9f7458
tbiliso <vakhoj@gmail.com>
2026-09-13 21:31:06 +0400

    feat: handle login form submit

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,19 @@
 import '../css/Login.css'
+import { useState } from 'react'
 function LogIn() {
+    const [password, setPassword] = useState('')
+    const [user, setUser] = useState('')
+    const handleSubmit = (e) =>{
+        e.preventDefault()
+        console.log(user, password)
+    }
     return (
         <div className="login">
             <h1 className='login-title'>Home Library <br /> Organizer</h1>
-            <form className="login-form">
+            <form className="login-form" onSubmit={handleSubmit}>
                 <div className="login-input">
-                    <input type="text" placeholder="User Name" id="login-username" />
-                    <input type="password" placeholder="Password" id="login-password" />
+                    <input type="text" placeholder="User Name" id="login-username" onChange={(e)=>setUser(e.target.value)}/>
+                    <input type="password" placeholder="Password" id="login-password" onChange={(e)=>setPassword(e.target.value)}/>
                 </div>
                 <button type="submit" className="login-submit">Log In</button>
             </form>
