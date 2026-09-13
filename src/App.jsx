e048d3d466f6470c501716ac277be56a6a7ae2b5
tbiliso <vakhoj@gmail.com>
2026-09-13 20:19:04 +0400

    fix: toggle stiles and title

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react';
 import LogIn from './pages/Login.jsx'
+import './App.css'
 function App() {
 
   const [font, setFont] = useState('HarryPotter')
   
   return (
-    <div style={{fontFamily:font}}>
-      <input type="checkbox"  className='fontToggle' onChange={(e)=>setFont(e.target.checked ? "HarryPotter":"Arial")}/>
+    <div style={{fontFamily:font, backgroundColor: 'rgb(203, 209, 146)'} }>
+      <input type="checkbox" title='Change Font'  className='fontToggle' onChange={(e)=>setFont(e.target.checked ? "HarryPotter":"Arial")}/>
       <LogIn />
     </div>
   );
