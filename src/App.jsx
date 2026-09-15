62400bcfbf28f3428ab4b3dc3b2cfeebc5758594
tbiliso <vakhoj@gmail.com>
2026-09-16 00:19:21 +0400

    feat: add routes and fix styles

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,18 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 function App() {
 
   const [font, setFont] = useState('HarryPotter')
-  
+
   return (
-    <div style={{fontFamily:font, backgroundColor: 'rgb(203, 209, 146)'} }>
-      <input type="checkbox" title='Change Font'  className='fontToggle' onChange={(e)=>setFont(e.target.checked ? "HarryPotter":"Arial")}/>
-      <LogIn />
-      <Register />
+    <div className='mainWrapper' style={{ fontFamily: font}}>
+      <input type="checkbox" title='Change Font' className='fontToggle' onChange={(e) => setFont(e.target.checked ? "HarryPotter" : "Arial")} />
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<LogIn />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
+      
     </div>
   );
 }
