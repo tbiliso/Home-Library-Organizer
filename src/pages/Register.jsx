d40847273378b2602737759fb53487aff93ba774
tbiliso <vakhoj@gmail.com>
2026-09-17 00:26:37 +0400

    feat: add validation error messages

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -6,23 +6,68 @@ function Register() {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const [rptPass, setRptPass] = useState('')
-    const subForm =(e)=>{
+    const [errors, seterrors] = useState({
+        fname: '',
+        lname: '',
+        email: '',
+        password: '',
+        rptPass: ''
+    })
+    const subForm = (e) => {
         e.preventDefault()
-        if(!fname.trim() || !lname.trim() || !email.trim() || !password.trim() || !rptPass.trim() || password.trim() !== rptPass.trim()){
+
+        const newErrors = {
+            fname: '',
+            lname: '',
+            email: '',
+            password: '',
+            rptPass: ''
+        }
+        if (!fname.trim()) {
+            newErrors.fname = ('გთხოვთ შეიყვანოთ სახელი');
+        }
+
+        if (!lname.trim()) {
+            newErrors.lname = ('გთხოვთ შეიყვანოთ გვარი');
+        }
+        if (!email.trim()) {
+            newErrors.email = ('გთხოვთ შეიყვანოთ ელ-ფოსტა');
+        }
+        if (!password.trim()) {
+            newErrors.password = ('გთხოვთ შეიყვანოთ პაროლი');
+        }
+        if (!rptPass.trim()) {
+            newErrors.rptPass = ('გთხოვთ გაიმეოროთ პაროლი')
+        } else if (password !== rptPass) {
+            newErrors.rptPass = 'პაროლები არ ემთხვევა';
+        }
+        seterrors(newErrors);
+        if (
+            newErrors.fname ||
+            newErrors.lname ||
+            newErrors.email ||
+            newErrors.password ||
+            newErrors.rptPass
+        ) {
             return
         }
-        console.log(fname, lname, email, password, rptPass)
+        console.log(fname, lname, email, password, rptPass);
     }
     return (
         <form onSubmit={subForm} className="regForm">
-            <input type="text" 
-                    value={fname}
-                    placeholder="First name" 
-                    onChange={(e) => setFname(e.target.value)} />
+            <input type="text"
+                value={fname}
+                placeholder="First name"
+                onChange={(e) => setFname(e.target.value)} />
+            <p className="errorMsg">{errors.fname}</p>
             <input type="text" value={lname} placeholder="Last name" onChange={(e) => setLname(e.target.value)} />
+            <p className="errorMsg">{errors.lname}</p>
             <input type="email" value={email} placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+            <p className="errorMsg">{errors.email}</p>
             <input type="password" value={password} placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+            <p className="errorMsg">{errors.password}</p>
             <input type="password" value={rptPass} placeholder="Repeat password" onChange={(e) => setRptPass(e.target.value)} />
+            <p className="errorMsg">{errors.rptPass}</p>
             <button type="submit">Register</button>
         </form>
     )
